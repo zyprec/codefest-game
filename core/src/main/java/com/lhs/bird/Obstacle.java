8072097de83821e1b859aabb1b640a3ff469e4aa
zyprec <75097644+zyprec@users.noreply.github.com>
2021-02-04 16:56:39 -0500

    object classes

---
+++ core/src/main/java/com/lhs/bird/Obstacle.java
@@ -0,0 +1,11 @@
+package com.lhs.bird;
+
+import org.mini2Dx.core.graphics.Sprite;
+
+public class Obstacle extends Object {
+
+    public Obstacle(int _x, int _y, Sprite _sprite) {
+        super(_x, _y, _sprite);
+    }
+    
+}
---
+++ core/src/main/java/com/lhs/bird/Player.java
@@ -0,0 +1,11 @@
+package com.lhs.bird;
+
+import org.mini2Dx.core.graphics.Sprite;
+
+public class Player extends Object {
+
+    public Player(int _x, int _y, Sprite _sprite) {
+        super(_x, _y, _sprite);
+    }
+    
+}
