cf3e251a331384914a7ebef561c91080f624bf56
Justinas <justinassanders@gmail.com>
2021-02-11 15:22:37 -0500

    Update Player.java

---
+++ core/src/main/java/com/lhs/bird/Player.java
@@ -0,0 +1,12 @@
+package com.lhs.bird;
+
+import org.mini2Dx.core.graphics.Sprite;
+
+public class Player extends Object {
+
+    public Player(int _x, int _y, Sprite _sprite) {
+        super(_x, _y, _sprite);
+        hi
+    }
+    
+}
