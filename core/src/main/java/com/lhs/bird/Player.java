200e32f99c7ce7fb55d6f662778e8f04d01795da
Justinas <justinassanders@gmail.com>
2021-02-11 23:17:35 -0500

    Update Player.java

--- core/src/main/java/com/lhs/bird/Player.java
+++ core/src/main/java/com/lhs/bird/Player.java
@@ -6,7 +6,31 @@ public class Player extends Object {
 
     public Player(int _x, int _y, Sprite _sprite) {
         super(_x, _y, _sprite);
-        hi
+        int speedx = 100;
+        int speedy = 100;
+        int health = 5;
     }
     
+    public void setSpeed(int speedx, int speedy){
+        this.speedx = speedx;
+        this.speedy = speedy;
+    }
+
+    public int changeHealthDown(){
+        int health -= 1;
+        return health;
+    }
+
+    public int changeHealthUp(){
+        int health += 1;
+        return health;
+    }
+
+    public static String playerInput(){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("User Input: ");
+        str output = reader.nextString();
+        return output;
+    }
+
 }
