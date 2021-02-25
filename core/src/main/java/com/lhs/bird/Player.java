60563e70344e32b3901e79c8593cf1e2e50d9ed0
Justinas <justinassanders@gmail.com>
2021-02-24 21:52:26 -0500

    Update Player.java

--- core/src/main/java/com/lhs/bird/Player.java
+++ core/src/main/java/com/lhs/bird/Player.java
@@ -1,12 +1,42 @@
 package com.lhs.bird;
 
+import java.util.Scanner;
+
 import org.mini2Dx.core.graphics.Sprite;
 
 public class Player extends Object {
 
+    private int speedx;
+    private int speedy;
+    private int health;
+
     public Player(int _x, int _y, Sprite _sprite) {
         super(_x, _y, _sprite);
-        hi
+        int speedx = 100;
+        int speedy = 100;
+        int health = 5;
     }
     
+    public void setSpeed(int speedx, int speedy){
+        this.speedx = speedx;
+        this.speedy = speedy;
+    }
+
+    public int changeHealthDown(int health){
+        health -= 1;
+        return health;
+    }
+
+    public int changeHealthUp(int health){
+        health += 1;
+        return health;
+    }
+
+    public static String playerInput(){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("User Input: ");
+        String output = reader.nextLine();
+        return output;
+    }
+
 }
