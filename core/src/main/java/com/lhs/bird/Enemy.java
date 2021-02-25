1e2ca3018376f3d6a79e3f383bb74fc2b43f5517
kishanbava <78565148+kishanbava@users.noreply.github.com>
2021-02-24 22:18:46 -0500

    Update Enemy.java

--- core/src/main/java/com/lhs/bird/Enemy.java
+++ core/src/main/java/com/lhs/bird/Enemy.java
@@ -2,11 +2,32 @@ package com.lhs.bird;
 
 import org.mini2Dx.core.graphics.Sprite;
 
+import java.Player;
+
 public class Enemy extends Object {
+
+    private int speedx;
+    private int speedy;
+    private int damage;
 
     public Enemy(int _x, int _y, Sprite _sprite) {
         super(_x, _y, _sprite);
+        int speedx = 100;
+        int speedy = 100;
+        int damage = 1;
         
     }
+
+    public void setSpeed(int speedx, int speedy){
+        this.speedx = speedx;
+        this.speedy = speedy;
+    }
+
+    public void doDamage(){
+        changeHealthDown();
+        
+    }
+
+    
     
 }
